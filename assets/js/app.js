65088e29807b88544608a824bf4d6236e9288d8f
Admin SDN 24 <sdn24bandaaceh@gmail.com>
2026-08-04 17:40:17 +0700

    fix :api 4

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 const App = {
   // PENTING: Ganti URL ini dengan URL eksekusi Web App dari Google Apps Script Anda!
   // Contoh: 'https://script.google.com/macros/s/AKfycb.../exec'
-  API_URL: 'https://script.google.com/macros/s/AKfycbyTz21eXrnbxJNF58xXCqHMy5Nqe_2sd73UvTdGdy-1OOeWINTr0NL0RYHGN-3R44m9dw/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzxPksuYKNasUv2TmRBKMyTZtpm0VRG615wZIcRiXL6vORnOVNsQw4wsukKpos3z280/exec',
 
   init() {
     this.initDarkMode();
